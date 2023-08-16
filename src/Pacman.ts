dd3653fb74a265f40a7290d61bff079700d2db56
Jye Harry <jyeharry21@hotmail.com>
2023-08-16 16:49:05 +1000

    Update Pacman class

--- src/Pacman.ts
+++ src/Pacman.ts
@@ -1,18 +1,16 @@
 import { Direction, Position, Turn } from "./types";
 
-class Pacman {
+export default class Pacman {
   private position: Position | null;
-  private MAX_BOUND;
-  private MIN_BOUND;
+  private GRID_SIZE;
 
-  constructor(minBound: number, maxBound: number) {
-    this.MIN_BOUND = minBound;
-    this.MAX_BOUND = maxBound;
+  constructor(gridSize: number) {
+    this.GRID_SIZE = gridSize;
     this.position = null;
   }
 
   place(position: Position) {
-    if (!this.isWithinBounds(position)) return;
+    if (!this.#isWithinBounds(position)) return;
     this.position = position;
   }
 
@@ -21,52 +19,51 @@ class Pacman {
 
     switch (this.position.direction) {
       case Direction.NORTH:
-        if (this.position.y < this.MAX_BOUND) {
-          this.position.y++;
-        }
+        if (this.position.y < this.GRID_SIZE) this.position.y++;
         break;
       case Direction.EAST:
-        if (this.position.x < this.MAX_BOUND) {
-          this.position.x++;
-        }
+        if (this.position.x < this.GRID_SIZE) this.position.x++;
         break;
       case Direction.SOUTH:
-        if (this.position.y > this.MIN_BOUND) {
-          this.position.y++;
-        }
+        if (this.position.y > 0) this.position.y--;
         break;
       case Direction.WEST:
-        if (this.position.x > this.MIN_BOUND) {
-          this.position.x++;
-        }
+        if (this.position.x > 0) this.position.x--;
         break;
     }
   }
 
   left() {
-    this.changeDirection(Turn.LEFT);
+    this.#changeDirection(Turn.LEFT);
   }
 
   right() {
-    this.changeDirection(Turn.RIGHT);
+    this.#changeDirection(Turn.RIGHT);
   }
 
   report() {
     if (this.position)
-      return `${this.position.x}, ${this.position.y}, ${this.position.direction}`;
+      console.log(
+        [
+          this.position.x,
+          this.position.y,
+          Direction[this.position.direction],
+        ].join(", ")
+      );
   }
 
-  private isWithinBounds({ x, y, direction }: Position): boolean {
+  // private methods
+  #isWithinBounds({ x, y, direction }: Position): boolean {
     return (
-      x > this.MIN_BOUND &&
-      x < this.MAX_BOUND &&
-      y > this.MIN_BOUND &&
-      y < this.MAX_BOUND &&
+      x > 0 &&
+      x < this.GRID_SIZE &&
+      y > 0 &&
+      y < this.GRID_SIZE &&
       Object.values(Direction).includes(direction)
     );
   }
 
-  private changeDirection(turn: Turn) {
+  #changeDirection(turn: Turn) {
     if (!this.position) return;
     // enums are reverse mapped, so must divide by 2 to get actual length
     const directionsLength = Object.values(Direction).length / 2;
